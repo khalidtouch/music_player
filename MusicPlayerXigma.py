564b10277c0b629d4a62c7290bbefb389d568dbe
khalidtouch <46427666+khalidtouch@users.noreply.github.com>
2019-08-18 01:15:44 +0100

    Add files via upload
    The pause and play buttons now work as required

--- MusicPlayerXigma.py
+++ MusicPlayerXigma.py
@@ -20,9 +20,19 @@ text.pack()
 def browse_file():
     global filename
     filename = filedialog.askopenfile()
+    loadThenPlay(filename)
+    
 
 def about_us():
     messagebox.showinfo('Cosmic Music', 'This Music Player was designed by Xigma Interns 2019.')
+
+def notLoaded(any_file):
+    if any_file is None:
+        return True
+
+
+def printFilename(event):
+    print(filename)
 
         # MenuBar
 menubar = Menu(window)
@@ -54,20 +64,40 @@ menubar.add_cascade(label = 'Exit', command = window.destroy)
 
 
     # control functions
-def play_button():
+pause = False
 
+def loadThenPlay(any_file):
+    mixer.music.load(any_file)
+    mixer.music.play()
+    statusbar['text'] = 'Music Now Playing'
+
+def resume_music():
+    global pause_state
+    mixer.music.unpause()
+    pause_state = False
+
+def pause_music(event):
+    mixer.music.pause()
+    pause_state = True
+
+def music_stopped():
+    if statusbar['text'] == 'Music Stopped':
+        return True
+
+def play_button(event):
+    statusbar['text'] = 'Now Playing... '
     try:
-        mixer.music.load(filename)
-        mixer.music.play()
-        statusbar['text'] = 'Music Now Playing'
-        statusbar['anchor'] = W
-    except:
+        resume_music()
+        if music_stopped():
+            loadThenPlay(filename)
+    
+    except: 
         messagebox.showerror('Cosmic Music','File not selected')
 
 
 
     # stop button
-def stop_button():
+def stop_button(event):
     #print('stop')
     mixer.music.stop()
     statusbar['text'] = 'Music Stopped'
@@ -86,19 +116,37 @@ def volume_control(val):
 
 
 
+
                                             # play button
-play_photo = PhotoImage(file = ("C:/Users/USER/Pictures/play-button-2.png"))
-stop_photo = PhotoImage(file = ('C:/Users/USER/Pictures/stop-button.png'))
+play_photo = PhotoImage(file = ("C:/Users/USER/Pictures/play-button.png"))
+stop_photo = PhotoImage(file = ('C:/Users/USER/Pictures/stop.png'))
+pause_photo = PhotoImage(file = ('C:/Users/USER/Pictures/pause.png'))
 
 #adding a widget to make it clickable
-play_Button = Button(window, image = play_photo, command = play_button)
+play_Button = Button(window, image = play_photo)
+play_Button.bind('<Button-1>', play_button)
+play_Button.bind('<Button-2>', resume_music)
+play_Button.bind('<Button-3>', resume_music)
 play_Button.pack()
-stop_Button = Button(window, image = stop_photo, command = stop_button)
+
+pause_Button = Button(window, image = pause_photo)
+pause_Button.bind('<Button-1>', pause_music)
+pause_Button.bind('<Button-2>', pause_music)
+pause_Button.bind('<Button-3>', pause_music)
+pause_Button.pack()
+
+stop_Button = Button(window, image = stop_photo)
+stop_Button.bind('<Button-1>', stop_button)
+stop_Button.bind('<Button-2>', stop_button)
+stop_Button.bind('<Button-3>', printFilename)
 stop_Button.pack()
+
+
 volume_control = Scale(window, from_ = 0, to = 100, command = volume_control, orient = HORIZONTAL)
 volume_control.pack()
 volume_control.set(15)
 mixer.music.set_volume(15)
+
 
 
 statusbar = Label(window, text = 'Cosmic Music App. Copyright 2019', relief = SUNKEN, anchor = CENTER)
